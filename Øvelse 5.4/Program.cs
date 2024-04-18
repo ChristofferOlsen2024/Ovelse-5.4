6d176afec1f991a8e636209d69c3b35dfb75494d
Skole <Skole@DESKTOP-87FGBUN>
2024-04-18 08:18:25 +0200

    done

--- Øvelse 5.4/Program.cs
+++ Øvelse 5.4/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string adgangskode;
-Console.WriteLine("Skriv koden her.");
 do
 {
     adgangskode = Console.ReadLine();
-} while (adgangskode != "koden");
+} while (adgangskode != "password");
